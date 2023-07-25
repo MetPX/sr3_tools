169577fcdfc30c65d1631338d0c8a7610a02918f
Reid Sunderland <reid.sunderland@ssc-spc.gc.ca>
2023-07-25 18:57:13 +0000

    Improve Bash completions

--- completion/sr3_tools_completion.bash
+++ completion/sr3_tools_completion.bash
@@ -1,7 +1,81 @@
 # Bash (tab) completion script for sr3_tools
 
 SARRA_COMPONENTS="cpost flow poll post watch sarra sender shovel subscribe winnow"
+SARRA_COMPONENTS_RE="^(${SARRA_COMPONENTS// /|})\$"
 SR3_ACTIONS="cleanup declare disable enable list restart run sanity setup show start status stop"
+
+###
+# component_config
+# 
+# Generate component/config completion
+#
+# Separator is configurable so the same code can be reused for
+# logs (component_configname) and sr3d (component/config)
+#
+# Potential completions:
+#   - ${component}${sep} prefix
+#   - ${component}${sep} + config file names, excluding .conf
+#       either from ${PWD} or from ${PWD}/${component}
+#
+# Arguments:
+#   $1 = the separator character (should be _ or /)
+###
+component_config()
+{
+    sep="$1"
+    pwd_before="${PWD}"
+    cdir="${PWD##*/}"
+    
+    # Config file name case, when the user already has the full ${component}${sep} (and maybe part of 
+    # the config file name) in their COMP_CWORD
+    # Must be the full component name and sep. 
+
+    # cut off everything from the current word after and including $sep
+    maybe_component=${COMP_WORDS[${COMP_CWORD}]%%${sep}*}
+    if [[ "${maybe_component}" =~ ${SARRA_COMPONENTS_RE} ]] && [[ "${COMP_WORDS[${COMP_CWORD}]}" == "${maybe_component}${sep}"* ]]; then
+        # when the user is one level up from the $component config directory, but they
+        # have ${component}${sep}, cd to that directory then complete using config files inside
+        if [[ "${SARRA_COMPONENTS}" != *"${cdir}"* ]]; then
+            #maybe_component=${COMP_WORDS[${COMP_CWORD}]%%${sep}*}
+            if [[ "${SARRA_COMPONENTS}" == *"${maybe_component}"* ]] && \
+               [ -d "${maybe_component}" ]; then
+                cd "${maybe_component}" || return
+                cdir="${PWD##*/}"
+            fi
+        fi
+        # part of current word after $component$sep
+        current=${COMP_WORDS[${COMP_CWORD}]/"${cdir}${sep}"/""}
+        # % to match at the end of the string, otherwise there will be issues with some config names
+        # like a sender named send_to_dest or a poll named poll_this_source
+        prefix=${COMP_WORDS[${COMP_CWORD}]/%"${current}"/""}
+        mapfile -t tempreply < <(compgen -A file -X '!*.conf' -- "${current}")
+        COMPREPLY=()
+        for option in "${tempreply[@]}"; do
+            # Could append _*.log here
+            COMPREPLY+=("${prefix}${option%%.conf}")
+        done
+    
+    # Case where there's no $component$sep prefix yet
+    else
+        # If inside a $component directory, use that.
+        # when the pump name == a component name, this won't work.
+        if [[ "${SARRA_COMPONENTS}" == *"${cdir}"* ]]; then
+            # not really necessary, there shouldn't be more than one option, could set the reply to cdir
+            mapfile -t tempreply < <(compgen -W "${cdir}" "${COMP_WORDS[${COMP_CWORD}]}")
+        # Otherwise, offer all matching options
+        else
+            mapfile -t tempreply < <(compgen -W "${SARRA_COMPONENTS}" "${COMP_WORDS[${COMP_CWORD}]}")
+        fi
+
+        COMPREPLY=()
+        for option in "${tempreply[@]}"; do
+            COMPREPLY+=("${option}${sep}")
+        done
+    fi
+
+    cd "${pwd_before}" || return
+}
+
 
 ###
 # sr3d
@@ -19,14 +93,8 @@ _sr3d()
         for option in "${tempreply[@]}"; do
             COMPREPLY+=("${option} ")
         done
-    fi
-    # component/config argument
-    if [ "${COMP_CWORD}" == 2 ]; then
-        mapfile -t tempreply < <(compgen -W "${SARRA_COMPONENTS}" "${COMP_WORDS[${COMP_CWORD}]}")
-        COMPREPLY=()
-        for option in "${tempreply[@]}"; do
-            COMPREPLY+=("${option}/")
-        done
+    else
+        component_config '/'
     fi
 }
 
@@ -34,48 +102,11 @@ complete -o nospace -F _sr3d sr3d
 
 ###
 # sr3l
-# Potential completions:
-#   - the current $component directory (log file prefix)
-#   - the name of a config file from the current or $component directory
+# See component_config function
 ###
 _sr3l()
 {
-    pwd_before="${PWD}"
-    # Component prefix case
-    cdir="${PWD##*/}"
-    if [[ "${SARRA_COMPONENTS}" == *"${cdir}"* ]] && \
-       [[ "${COMP_LINE}" != *"${cdir}"* ]]; then
-        mapfile -t tempreply < <(compgen -W "${cdir}" "${COMP_WORDS[${COMP_CWORD}]}")
-        COMPREPLY=()
-        # Loop isn't really necessary, there shouldn't be more than one option
-        for option in "${tempreply[@]}"; do
-            COMPREPLY+=("${option}_")
-        done
-    # Config file name case
-    else
-        # when the user is one level up from the $component config directory, but
-        # they've manually typed $component_, complete from config files in that directory
-        if [[ "${SARRA_COMPONENTS}" != *"${cdir}"* ]]; then 
-            maybe_component=${COMP_WORDS[${COMP_CWORD}]%%_*}
-            if [[ "${SARRA_COMPONENTS}" == *"${maybe_component}"* ]] && \
-               [ -d "${maybe_component}" ]; then
-                cd "${maybe_component}" || return
-                cdir="${PWD##*/}"
-            fi
-        fi
-        
-        current=${COMP_WORDS[${COMP_CWORD}]/"${cdir}_"/""}
-        # % to match at the end of the string, otherwise there will be issues with some config names
-        # like a sender named send_to_dest or a poll named poll_this_source
-        prefix=${COMP_WORDS[${COMP_CWORD}]/%"${current}"/""}
-        mapfile -t tempreply < <(compgen -A file -X '!*.conf' -- "${current}")
-        COMPREPLY=()
-        for option in "${tempreply[@]}"; do
-            # Could append _*.log here
-            COMPREPLY+=("${prefix}${option%%.conf}")
-        done
-    fi
-    cd "${pwd_before}" || return
+    component_config '_'
 }
 
 complete -o nospace -F _sr3l sr3l
